c8cc7d7794fee0f9d04f3b87011d1af18d8b85ca
seungchan.moon <moon.unimewo@gmail.com>
2022-09-04 19:01:21 +0900

    feat : Querydsl 설정 및 실행

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,7 @@ package hello.itemservice;
 
 import hello.itemservice.config.JpaConfig;
 import hello.itemservice.config.MyBatisConfig;
+import hello.itemservice.config.QuerydslConfig;
 import hello.itemservice.config.SpringDataJpaConfig;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
@@ -21,7 +22,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
 //@Import(MyBatisConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @Slf4j
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
